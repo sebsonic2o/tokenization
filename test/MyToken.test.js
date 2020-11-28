2245c961dfc3ab5f4d1e771c42302a1d70b9ea7d
Sebastien Cheraux <scheraux@gmail.com>
2020-11-28 15:20:52 -0800

    add unit test using chai expect and chai-as-promised

---
+++ test/MyToken.test.js
@@ -0,0 +1,20 @@
+const Token = artifacts.require("./MyToken.sol");
+
+var chai = require("chai");
+
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract("Token Test", async accounts => {
+  it("should have all tokens in my account", async () => {
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+  });
+});
